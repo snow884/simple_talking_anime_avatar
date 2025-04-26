461d32e6d9abadfff5106ba3091fbba680ffc143
snow884 <92733886+aivansky-contractor@users.noreply.github.com>
2025-04-26 19:57:00 -0400

    add test.py

--- test.py
+++ test.py
@@ -2,8 +2,8 @@
 
 import cv2
 
-from .src.simple_talking_anime_avatar.avatar_gen import get_image_speaking
-from .src.simple_talking_anime_avatar.speech_to_vid import get_speaker_video
+from src.simple_talking_anime_avatar.avatar_gen import get_image_speaking
+from src.simple_talking_anime_avatar.speech_to_vid import get_speaker_video
 
 image_avatar_speaking_u = get_image_speaking("u")
 cv2.imwrite("test.png", image_avatar_speaking_u)
